08d1b4cb8325313b1f8b5b3d6412279eef04aa19
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 17:26:06 -0700

    Turned dictionary into if/else statement for finding operation

--- calculator.py
+++ calculator.py
@@ -13,41 +13,70 @@ def calculate():
     Prompt user for operation, call function imported from arithmetic,
     and print result.
     """
+    token_list = "'+' '-'' '*' '/' 'square' 'cube' 'pow' 'or' 'mod'"
+    print "Enter an operation or enter 'q' to quit."
+    print "Valid operations include {}".format(token_list)
+
     while True: 
-        prompt = "Enter an operation or enter 'q' to quit."
-        num_to_calc = raw_input(prompt)
+        num_to_calc = raw_input("> ")
         if num_to_calc.lower() == 'q': #if the first token is Q, quit
             return
-        else:
+        else: 
             tokens = num_to_calc.split()
-            # check validity of input
-            if len(tokens) == 3:
-                func_name = tokens[0]
-                num1 = float(tokens[1])
+            func_name = tokens[0]
+            num1 = float(tokens[1])
+            if len(tokens) > 2:
                 num2 = float(tokens[2])
-            elif len(tokens) == 2: 
-                func_name = tokens[0]
-                num1 = float(tokens[1])
-                num2 = 1
             else:
-                print "That is not a valid entry."
-                continue # go to the top of the while loop
+                num2 = 1
+
+            if func_name == '+':
+                print add(num1, num2)
+            elif func_name == '-':
+                print subtract(num1, num2)
+            elif func_name == '*':
+                print multiply(num1, num2)
+            elif func_name == '/':
+                print divide(num1, num2)
+            elif func_name == 'square':
+                print square(num1)
+            elif func_name == 'cube':
+                print cube(num1)
+            elif func_name == 'pow':
+                print power(num1, num2)
+            elif func_name == 'mod':
+                print mod(num1, num2)
+            else:
+                print "First element must be one of the following:"  
+                print token_list
+                continue 
+
+            # # check validity of input
+            # if len(tokens) == 3:
+            #     func_name = tokens[0]
+            #     num1 = float(tokens[1])
+            #     num2 = float(tokens[2])
+            # elif tokens[0] == 'square' or tokens[0] == 'cube':
+
+            # else:
+            #     print "That is not a valid entry."
+            #     continue # go to the top of the while loop
 
 
             # create a dictionary with keys = operations
             # make global and pass to calculate???
-            operations = {'+': add(num1, num2),
-                          '-': subtract(num1, num2),
-                          '*': multiply(num1, num2),
-                          '/': divide(num1, num2),
-                          'square': square(num1),
-                          'cube': cube(num1),
-                          'pow': power(num1, num2),
-                          'mod': mod(num1, num2)}
+            # operations = {'+': add(num1, num2),
+            #               '-': subtract(num1, num2),
+            #               '*': multiply(num1, num2),
+            #               '/': divide(num1, num2),
+            #               'square': square(num1),
+            #               'cube': cube(num1),
+            #               'pow': power(num1, num2),
+            #               'mod': mod(num1, num2)}
                                                     
             # retrieve and call operation from dict
-            if func_name in operations:
-                print operations[func_name]
+            # if func_name in operations:
+            #     print operations[func_name]
 
             # if func_name == "+":
             #     print add(num1, num2)
